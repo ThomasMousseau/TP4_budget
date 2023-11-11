cdf33438bfc1f8e669cbd07a7d161983c69cca54
ThomasMousseau <univers220@gmail.com>
2023-11-11 09:46:42 -0500

    added pipe

--- myscript.py
+++ myscript.py
@@ -4,9 +4,8 @@ import subprocess
 bad_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 good_hash = 'bfdccab909c32635457d41eeb6e7fed322026170'
 
-os.system(f'git bisect start {bad_hash} {good_hash}')
-os.system('git bisect run python manage.py test')
-os.system('git bisect reset')
+os.system(f'git bisect start {bad_hash} {good_hash} 2>&1')
+os.system('git bisect run python manage.py test 2>&1')
+os.system('git bisect reset 2>&1')
 
-print('This is the final commit that broke the code:' + str(bad_hash))
 
