4effc2bcef78c560c31d59179865c6fa5d3dcaa0
ThomasMousseau <univers220@gmail.com>
2023-11-12 10:34:20 -0500

    can log be seen?

--- myscript.py
+++ myscript.py
@@ -1,9 +1,11 @@
 import os
 import subprocess
+import sys
 
 bad_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
 good_hash = 'bfdccab909c32635457d41eeb6e7fed322026170'
 
+sys.stdout.flush()
 os.system(f'git bisect start {bad_hash} {good_hash}')
 os.system('git bisect run python manage.py test')
 os.system('git bisect reset')
